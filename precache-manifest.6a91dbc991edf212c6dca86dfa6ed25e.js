2da0b68d50f31a99eaa11784c085b1b075fbff8e
Mwansa <Mwansad.mwango@gmail.com>
2020-07-05 00:19:48 +0800

    Updates

---
+++ precache-manifest.6a91dbc991edf212c6dca86dfa6ed25e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41a9a6905111a725e4acf6108e2e6fb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0db5b2fbead830eb4ad4",
+    "url": "/static/css/main.34c840c9.chunk.css"
+  },
+  {
+    "revision": "7bee2e79ea24a141a2ec",
+    "url": "/static/js/2.53ae2b15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.53ae2b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db5b2fbead830eb4ad4",
+    "url": "/static/js/main.a00493a7.chunk.js"
+  },
+  {
+    "revision": "2c9a44d3bc0818179c37",
+    "url": "/static/js/runtime-main.14dcacf9.js"
+  },
+  {
+    "revision": "baed0cce42bac8c3ccbd9b021f61978c",
+    "url": "/static/media/logo.baed0cce.png"
+  },
+  {
+    "revision": "f408c05d970646463e26cf6ea832e4c7",
+    "url": "/static/media/person.f408c05d.png"
+  }
+]);
